b884b8449c10dada0b37e59974640a96523f0a02
Fardeen Khan <fk7483136@gmail.com>
2025-10-19 20:16:07 +0530

    Fixed the Bug of /interview Page not showing up

--- app/(main)/interview/layout.js
+++ app/(main)/interview/layout.js
@@ -5,7 +5,6 @@ import { BarLoader } from 'react-spinners'
 const Layout = ({children}) => {
   return (
     <div className="px-5 h-[90vh]">
-       
         <Suspense fallback={<div className='flex justify-center items-center'><Loader/></div>}>
       {children}
         </Suspense>
